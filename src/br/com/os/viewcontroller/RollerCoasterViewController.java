31cfd132ba1213e297b36107acc8f4891e663bb2
Alan Jeferson <alan.jeferson11@gmail.com>
2016-03-22 21:53:45 -0300

    Some improvements on the RollerCoasterViewController

--- src/br/com/os/viewcontroller/RollerCoasterViewController.java
+++ src/br/com/os/viewcontroller/RollerCoasterViewController.java
@@ -4,10 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -31,15 +32,13 @@ public class RollerCoasterViewController extends JFrame implements ViewControlle
 	private JButton createButton;
 
 	// Constants
-	private static final int WINDOW_WIDTH = 600;
-	private static final int WINDOW_HEIGHT = 230;
 	private static final int INITIAL_TRAVELLING_TIME = 5;
 	private static final int INITIAL_SEATS = 2;
 
 	private ViewControllerDelegate itemHandler;
 
 	public RollerCoasterViewController() {
-		super("Nova Montanha Russa");
+		super("Novo Vagão");
 	}
 
 	// ViewController implement
@@ -47,10 +46,10 @@ public class RollerCoasterViewController extends JFrame implements ViewControlle
 	@Override
 	public void build(ViewControllerDelegate itemHandler) {
 		this.container = this.getContentPane();
-		this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		this.itemHandler = itemHandler;
 		this.setResizable(false);
 		this.addComponents();
+		this.pack();
 	}
 
 	@Override
@@ -72,111 +71,49 @@ public class RollerCoasterViewController extends JFrame implements ViewControlle
 	}
 
 	/** Addss all the components to the JFrame. */
-	private void addComponents() {
-		
-		JPanel centerPanel = new JPanel();
-		centerPanel.setLayout(new GridLayout(2, 1, 0, 20));
-		
-		// Panel travelling time
-		JPanel panelTravellingTime = new JPanel();
-		panelTravellingTime.setLayout(new GridLayout(2, 1, 0, 0));
+	private void addComponents() {		
 
-		JPanel panelTopTravellingTime = new JPanel();
-		panelTopTravellingTime.setLayout(new FlowLayout(FlowLayout.CENTER));
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-		panelTopTravellingTime.add(new JLabel("Tempo de viagem:"));
+		// Travelling time
+		JPanel panelTravelling = new JPanel();
+		panelTravelling.setLayout(new BoxLayout(panelTravelling, BoxLayout.Y_AXIS));
+		JPanel panelFlowTravelling = new JPanel();
+		panelFlowTravelling.setLayout(new FlowLayout(FlowLayout.CENTER));
+		panelFlowTravelling.add(new JLabel("Tempo de viagem:"));
+		this.textFieldTravellingTime = this.coolTextField();
+		panelFlowTravelling.add(this.textFieldTravellingTime);
+		panelTravelling.add(panelFlowTravelling);
+		this.sliderTravellingTime = this.coolSlider(5, 60, 5);
+		panelTravelling.add(this.sliderTravellingTime);
+		panel.add(panelTravelling);
 
-		this.textFieldTravellingTime = new JTextField(5);
-		this.textFieldTravellingTime.setEditable(false);
-		panelTopTravellingTime.add(this.textFieldTravellingTime);
+		panel.add(Box.createRigidArea(new Dimension(10, 20)));
 
-		panelTravellingTime.add(panelTopTravellingTime);
-
-		this.sliderTravellingTime = new JSlider(5, 60);
-		this.sliderTravellingTime.setPaintTicks(true);
-		this.sliderTravellingTime.setMajorTickSpacing(5);
-		this.sliderTravellingTime.setMinorTickSpacing(5);
-		this.sliderTravellingTime.setPaintLabels(true);
-		this.sliderTravellingTime.addChangeListener(this);
-		panelTravellingTime.add(this.sliderTravellingTime);
-		
-		// Panel Seats
+		// Leaving time
 		JPanel panelSeats = new JPanel();
-		panelSeats.setLayout(new GridLayout(2, 1, 0, 0));
-		
-		JPanel panelSeatsTop = new JPanel();
-		panelSeatsTop.setLayout(new FlowLayout(FlowLayout.CENTER));
-		
-		panelSeatsTop.add(new JLabel("Quantidade de assentos:"));
-		
-		this.textFieldSeats = new JTextField(5);
-		this.textFieldSeats.setEditable(false);
-		panelSeatsTop.add(this.textFieldSeats);
-		
-		panelSeats.add(panelSeatsTop);
-		
-		this.sliderSeats = new JSlider(2, 10);
-		this.sliderSeats.setPaintTicks(true);
-		this.sliderSeats.setMajorTickSpacing(2);
-		this.sliderSeats.setMinorTickSpacing(2);
-		this.sliderSeats.setSnapToTicks(true);
-		this.sliderSeats.setPaintLabels(true);
-		this.sliderSeats.addChangeListener(this);
+		panelSeats.setLayout(new BoxLayout(panelSeats, BoxLayout.Y_AXIS));
+		JPanel panelFlowSeats = new JPanel();
+		panelFlowSeats.setLayout(new FlowLayout(FlowLayout.CENTER));
+		panelFlowSeats.add(new JLabel("Número de assentos:"));
+		this.textFieldSeats = this.coolTextField();
+		panelFlowSeats.add(this.textFieldSeats);
+		panelSeats.add(panelFlowSeats);
+		this.sliderSeats = this.coolSlider(2, 10, 2);
 		panelSeats.add(this.sliderSeats);
-		
-		centerPanel.add(panelTravellingTime);
-		centerPanel.add(panelSeats);
-		
-		this.container.add(centerPanel, BorderLayout.CENTER);
+		panel.add(panelSeats);
 
-		this.createButton = new JButton("Criar");
+		panel.add(Box.createRigidArea(new Dimension(10, 30)));
+		
+		this.container.add(panel, BorderLayout.CENTER);
+
+		// Create Passenger Button
+		this.createButton = new JButton("Criar Vagão");
 		this.createButton.addActionListener(this);
 		this.container.add(this.createButton, BorderLayout.SOUTH);
 		
 		this.reset();
-		
-
-//		JPanel panel = new JPanel();
-//		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-//
-//		// Travelling time
-//		JPanel panelTravelling = new JPanel();
-//		panelTravelling.setLayout(new BoxLayout(panelTravelling, BoxLayout.Y_AXIS));
-//		JPanel panelFlowTravelling = new JPanel();
-//		panelFlowTravelling.setLayout(new FlowLayout(FlowLayout.CENTER));
-//		panelFlowTravelling.add(new JLabel("Tempo de embarque:"));
-//		this.textFieldTravellingTime = this.coolTextField();
-//		panelFlowTravelling.add(this.textFieldTravellingTime);
-//		panelTravelling.add(panelFlowTravelling);
-//		this.sliderTravellingTime = this.coolSlider();
-//		panelTravelling.add(this.sliderTravellingTime);
-//		panel.add(panelTravelling);
-//
-//		panel.add(Box.createRigidArea(new Dimension(10, 20)));
-//
-//		// Leaving time
-//		JPanel panelSeats = new JPanel();
-//		panelSeats.setLayout(new BoxLayout(panelSeats, BoxLayout.Y_AXIS));
-//		JPanel panelFlowSeats = new JPanel();
-//		panelFlowSeats.setLayout(new FlowLayout(FlowLayout.CENTER));
-//		panelFlowSeats.add(new JLabel("Tempo de embarque:"));
-//		this.textFieldSeats = this.coolTextField();
-//		panelFlowSeats.add(this.textFieldSeats);
-//		panelSeats.add(panelFlowSeats);
-//		this.sliderSeats = this.coolSlider();
-//		panelSeats.add(this.sliderSeats);
-//		panel.add(panelSeats);
-//
-//		panel.add(Box.createRigidArea(new Dimension(10, 20)));
-//		
-//		this.container.add(panel, BorderLayout.CENTER);
-//
-//		// Create Passenger Button
-//		this.createButton = new JButton("Criar Vag�o");
-//		this.createButton.addActionListener(this);
-//		this.container.add(this.createButton, BorderLayout.SOUTH);
-//		
-//		this.reset();
 
 
 	}
@@ -189,11 +126,11 @@ public class RollerCoasterViewController extends JFrame implements ViewControlle
 	}
 
 	/** Builds a setup slider */
-	private JSlider coolSlider() {
-		JSlider slider = new JSlider(1, 15, 1);
+	private JSlider coolSlider(int min, int max, int value) {
+		JSlider slider = new JSlider(min, max);
 		slider.setPaintTicks(true);
-		slider.setMajorTickSpacing(1);
-		slider.setMinorTickSpacing(1);
+		slider.setMajorTickSpacing(value);
+		slider.setMinorTickSpacing(value);
 		slider.setPaintLabels(true);
 		slider.setPreferredSize(new Dimension(500, 45));
 		slider.addChangeListener(this);
